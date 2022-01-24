60df1c31a5e0dedbdb4faaf979f96cb27e0818b2
Olesya555 <olet.ru@gmail.com>
2022-01-24 19:41:03 +0600

    Работа с вариантами оформления текста. Часть 2

--- lib/text_varianti_raboti.dart
+++ lib/text_varianti_raboti.dart
@@ -11,21 +11,54 @@ class TextDemoScreen extends StatelessWidget {
         // у MaterialApp дочка -  home: Scaffold(
         // а у Scaffold дочка - body: Container(
         body: Center( // помещаем в центр наш контейнер
-          child: Container(
-            width: 200, // задали ширину
-            height: 200, // задали  высоту
-            decoration: BoxDecoration(
-              color: Colors.lightBlue,
-                border: Border.all(color: Colors.blueAccent, // оформление рамки
-                    // задаём ширину рамки
-                    width: 5),
-            ),
-            alignment: Alignment.center, // Нахождение в центре контейнера
-            child: const Text('Просто длинный длинный длинный длинныйтекст',
-              // textDirection: TextDirection.ltr, // Задали направление текста
-              overflow: TextOverflow.clip, // Параметр обрезки текста
-              softWrap: true, // Делает перенос текста по словам обычно стоит по умолчанию true.
-            ),
+          child: Column( // Делаем колонку, в которую будем помещать контейнеры с текстом
+            children: [ // дети колонки - наши 2 контейнера
+              Container(
+                // Параметры контейнера и его украшательство
+                width: 200, // задали ширину
+                height: 200, // задали  высоту
+                margin: const EdgeInsets.only(top: 50), // задали отступ только сверху
+                decoration: BoxDecoration( // Оформляем контейнер
+                  color: Colors.lightBlue, // задали цвет для контейнера
+                    border: Border.all(color: Colors.blueAccent, // оформление рамки
+                        // задаём ширину рамки
+                        width: 5),
+                ),
+                alignment: Alignment.center, // Нахождение в центре контейнера
+
+                // Текст внутри контейнера
+                child: const Text('Просто длинный длинный длинный длинныйтекст',
+                  // textDirection: TextDirection.ltr, // Задали направление текста
+                  overflow: TextOverflow.clip, // Параметр обрезки текста
+                  softWrap: true, // Делает перенос текста по словам обычно стоит по умолчанию true.
+                  style: TextStyle(color: Colors.white, fontSize: 18), // сделали цвет текста белым
+                ),
+              ),
+
+              Container(
+                width: 200, // задали ширину
+                height: 200, // задали  высоту
+                margin: const EdgeInsets.only(top: 50), // задали отступ только сверху
+                decoration: BoxDecoration(
+                  color: Colors.lightBlue,
+                  border: Border.all(color: Colors.blueAccent, // оформление рамки
+                      // задаём ширину рамки
+                      width: 5),
+                ),
+                alignment: Alignment.center, // Нахождение в центре контейнера
+
+                // Пример Text.rich - Работа с "богатым" текстом
+                child: const Text.rich( // Пример Text.rich
+                  TextSpan(text: "Фрагмент", // Родительский элемент, которому мы прописываем общий стиль
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                      children: <TextSpan> [ // далее организуем "дочек" с изменениями в стиле
+                      TextSpan(text: " стилизованного", style: TextStyle(fontWeight: FontWeight.bold)), // полужирный текст
+                      TextSpan(text: " текста", style: TextStyle(fontStyle: FontStyle.italic)), // курсив
+                    ]
+                  )
+                ),
+              ),
+            ],
           ),
         ),
       ),
