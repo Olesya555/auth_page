2067aa27ab597482d8361060e295861c5fd35062
Olesya555 <olet.ru@gmail.com>
2022-01-28 17:08:32 +0600

    SnackBar i AppBar

--- lib/app_bar_screen.dart
+++ lib/app_bar_screen.dart
@@ -10,14 +10,33 @@ class AppBarScreen extends StatefulWidget {
 class _AppBarScreenState extends State<AppBarScreen> {
   @override
   Widget build(BuildContext context) {
+    // Пропишем стиль текстовой кнопки
+    final ButtonStyle buttonStyle =
+    TextButton.styleFrom(primary: Theme.of(context).colorScheme.onPrimary);
+
     return MaterialApp(
       home: Scaffold(
         //Создаем AppBar
         appBar: AppBar(
           // Прописываем заголовок AppBar, можем задать тексту стиль и параметры
           title: const Text('Демо AppBar'),
-          actions: <Widget> [
-            IconButton(onPressed: () {}, icon: const Icon(Icons.account_balance)),
+          actions: <Widget> [  // здесь прописываем "дочек" - наши иконки и
+            // текстовые кнопки. Они идут в массиве, списком. Скобки []
+            // вставляем кнопки-иконки, пока с заглушками на нажатие
+            IconButton( // Иконка-кнопка
+                tooltip: 'Баланс', // tooltip: подсказка, срабатывает при "зажатии" на кнопку
+                onPressed: () {},
+                icon: const Icon(Icons.account_balance)),
+
+            IconButton(onPressed: () {}, icon: const Icon(Icons.settings)),
+
+            //Текстовая кнопка, без иконки
+            TextButton(
+                onPressed: () {},
+                child: Text ('Профиль'),
+                style: buttonStyle, // стиль прописан в классе.
+            ),
+
           ],
         ),
         body: const Center(child: Text('Контент')),
