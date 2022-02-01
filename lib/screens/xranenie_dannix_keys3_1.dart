51fcc0038b80ebae0102d0d6bbe604a1e6915cfd
Olesya555 <olet.ru@gmail.com>
2022-02-01 17:07:25 +0600

    Кейс-задание 3.1 вроде получилось!!!!!!, добавила комментарии для себя

--- lib/screens/xranenie_dannix_keys3_1.dart
+++ lib/screens/xranenie_dannix_keys3_1.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -25,6 +27,7 @@ class _MyHomeState extends State<MyHome> {
 
   dynamic _name;
 
+
   @override
   void initState() {
     super.initState();// метод initState - происходит инициализация состояния и загрузка данных
@@ -36,23 +39,26 @@ class _MyHomeState extends State<MyHome> {
   void _loadName() async { // переменная, отвечающая за загрузку данных, идёт в режиме async (асинхронном)
     final prefs = await SharedPreferences.getInstance(); // получение ссылки на объект SharedPreferences (классическое получение)
     setState(() { // в методе setState происходит установка значения нашего счетчика, переменной _counter
-      _name = (prefs.getInt('name') ?? 0);  // с помощью классической конструкции null safety
+      _name = (prefs.getString('name') ?? 0);  // с помощью классической конструкции null safety
     }); // если у нас при первой загрузке, в счетчике еще не было значения, то преиенная получит значение "0"
   }
 
   // Сохранение данных по счетчику
   //Incrementing counter after click
-  void _incrementCounter() async { // приватная переменная "Увеличение счетчика" (_incrementCounter), идёт в асинхроне
+  void _newName() async { // приватная переменная "Увеличение счетчика" (_incrementCounter), идёт в асинхроне
     // потому что внутри у него используется ключевое слово await - ожидание, потому что операция м. занимать время, и нужно
     // дождаться завершения текущей операции, чтобы перейти к следующей
     final prefs = await SharedPreferences.getInstance(); // получение ссылки на объект SharedPreferences (классическое получение)
     // идет получение ссылки на результат счётчика
     setState(() { //и только потом выполняетя строка с командой "увеличение результата счетчика на +1" с защитой от  null
-      _name = (prefs.getInt('name') ?? 0); // если просто использовать getInt, то в случае пустого значения у нас
+      _name = (prefs.getString('name')); // если просто использовать getInt, то в случае пустого значения у нас
       // бы возникла ошибка, потому что прибавлять к значению null +1 это ошибка.
-      prefs.setInt('name', _name); // после увеличения счетчика мы его записываем в файл настроек с тем же самым ключом 'counter'
+      prefs.setString('name', _name); // после увеличения счетчика мы его записываем в файл настроек с тем же самым ключом 'counter'
     });
   }
+
+  final myController = TextEditingController();
+
 
   @override
   Widget build(BuildContext context) {
@@ -66,48 +72,59 @@ class _MyHomeState extends State<MyHome> {
         title: Text(widget.title),
       ),
       body: Center(
+        child: SingleChildScrollView(
         child: Column(
-            children: const [
+            children: [
 
-              SizedBox(height: 100,), // Отбивка
-
-              Text('Введите, пожалуйста, Ваше имя.',
+              const Text('Введите, пожалуйста, Ваш логин',
                 style: TextStyle(fontSize: 20, color: Colors.black),
                 textAlign: TextAlign.center, // Расположение текста по центру
               ),
 
-              SizedBox(height: 30,), // Отбивка
+              const SizedBox(height: 30,), // Отбивка
 
-              Padding(
+               Padding(
                 padding: EdgeInsets.symmetric(horizontal: 40.0),
                 child: TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                       filled: true,
-                      fillColor: Color(0xffeceff1),
+                      fillColor: Colors.white24,
                       enabledBorder: borderStyle1,
                       focusedBorder: borderStyle1,
                       ),
+                  controller: myController,
                 ),
               ),
-              SizedBox(height: 28,), // Отбивка
+              const SizedBox(height: 50,), // Отбивка
 
-              SizedBox(width: 180, height: 50, child:
-              ElevatedButton(onPressed: () {},
-                child: const Text('Войти'),
-                style: ElevatedButton.styleFrom(
-                  // Изменить размер текста внутри кнопки textStyle: const TextStyle(fontSize: 20),
-                    textStyle: const TextStyle(fontSize: 20),
-                    primary: Color(0xFF0079D0),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(36.0),
-                    )
-                ),
-              )
+              SizedBox(width: 180, height: 50,
+                  child: ElevatedButton(onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          // Retrieve the text the that user has entered by using the
+                          // TextEditingController.
+                          content: Text('Здравствуйте, $_name'),
+                        );
+                      },
+                    );
+                  }, child: const Text('Войти'),
+                      style: ElevatedButton.styleFrom(
+                      textStyle: const TextStyle(fontSize: 20),
+                  primary: Color(0xFF0079D0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(36.0),),
               ),
+      ),
+
+                ),
+
             ]
+        ),
         ),
       ),
     );
   }
 }
+
